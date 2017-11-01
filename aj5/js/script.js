639b07c856339683a1f9689b481cc86cc0f1150e
DenisVolhonsky <dvolhonsky@gmail.com>
2017-11-01 14:12:40 +0200

    method load

--- aj5/js/script.js
+++ aj5/js/script.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
     $('button').on('click', f1);
-
 });
 
 function f1() {
     $.getJSON('good1.json', function(data) {
         var out='';
         for(var key in data) {
+
+            out+='<div class="items">'
             out+='<b>Наименование товара: </b>'+ data[key].name +'<br>';
             out+='<b>Мощность: </b>'+data[key].power+'<br>';
             out+='<b>Вес: </b>'+data[key].weight+'<br>';
             out+='<img src="'+data[key].img+'"><br>';
-            console.log(out);
+            out+='<p class="out-file"></p><br></div>';
+
         }
         $('#info').html(out);
-        // $('#out-text').load("file.html");
+        $('.out-file').load('file.html');
 
     });
+
 }
 
 
